02c622a11832427d56dee196912f3262797d9b24
Ashley <you@example.com>
2018-03-07 15:29:32 +0300

    Add listings page structure

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var user (name, contact, location, listing, identity) { 
+       this.name=name;
+       this.contact=contact;
+       this.location=location;
+       this.listing=[true/false];
+       this.identity=identityNo;
+	   
+}
+
+var person (firstName , lastName ) {
+	this.firstName=firstName;
+	this.lastName=lastName;
+}
+
+var contact (phoneNo , email , phyadd, school ) {
+	this.phoneNo=number;
+	this.email=email;
+	this.phyadd=phyadd;
+	this.school=school;
+
+}
+var listing=location , cost , image , name){
+ 	this.location= mnlocation;
+ 	this.cost=cost;
+ 	this.image=image;
+ 	this.name=pname;
+ }
